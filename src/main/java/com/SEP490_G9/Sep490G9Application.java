6337bc325842ede85f111a4971cc17dcb7e0cf61
nam10701 <tuanldhe140929@fpt.edu.vn>
2023-01-14 22:26:05 +0700

    setup commit

---
+++ src/main/java/com/SEP490_G9/Sep490G9Application.java
@@ -0,0 +1,13 @@
+package com.SEP490_G9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep490G9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep490G9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SEP490_G9/Sep490G9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEP490_G9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490G9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
