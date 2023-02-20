34678fed21aa1f3e26e9f5555f569136f2d16330
toanvocuc <122213332+toanvocucc@users.noreply.github.com>
2023-02-20 20:56:54 +0700

    fix a bit

--- src/main/frontend/src/app/services/cart.service.ts
+++ src/main/frontend/src/app/services/cart.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Cart } from '../DTOS/Cart';
 const baseUrl = "http://localhost:9000/private/cart"
 export class CartService {
- 
-  constructor() { }
+  public getAllProduct(): Observable<Cart[]> {
+   
+        return this.ht    
+  }
+  
+  constructor(private httpClient: HttpClient) { }
 }
