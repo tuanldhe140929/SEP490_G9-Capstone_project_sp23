b484526438f4cb6656d8f549bfdc7630acc80de8
toanvocuc <122213332+toanvocucc@users.noreply.github.com>
2023-03-14 22:46:10 +0700

    amen

---
+++ src/test/java/com/SEP490_G9/repositories/CartRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.SEP490_G9.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.SEP490_G9.configs.TestConfig;
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@Import(TestConfig.class)
+class CartRepositoryTest {
+
+	@Test
+	void testget() {
+		fail("Not yet implemented");
+	}
+
+}
