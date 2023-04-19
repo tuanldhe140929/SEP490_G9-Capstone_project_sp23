df10bde1a158eb559ad623c29e079c880a0af0b1
nam10701 <namdhhe150519@fpt.edu.vn>
2023-04-19 19:16:34 +0700

    xoa các class thừa

--- src/test/java/com/SEP490_G9/services/SellerServiceTest.java
+++ src/test/java/com/SEP490_G9/services/SellerServiceTest.java
@@ -2,20 +2,28 @@ package com.SEP490_G9.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.exceptions.misusing.InvalidUseOfMatchersException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.SEP490_G9.common.Constant;
@@ -25,13 +33,18 @@ import com.SEP490_G9.entities.Product;
 import com.SEP490_G9.entities.Role;
 import com.SEP490_G9.entities.Seller;
 import com.SEP490_G9.entities.User;
+import com.SEP490_G9.entities.UserDetailsImpl;
 import com.SEP490_G9.repository.AccountRepository;
 import com.SEP490_G9.repository.ProductRepository;
 import com.SEP490_G9.repository.RoleRepository;
 import com.SEP490_G9.repository.SellerRepository;
 import com.SEP490_G9.repository.UserRepository;
+import com.SEP490_G9.service.SellerService;
 import com.SEP490_G9.service.serviceImpls.AccountServiceImpl;
 import com.SEP490_G9.service.serviceImpls.SellerServiceImpl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
@@ -44,10 +57,10 @@ class SellerServiceTest {
 	@Mock
 	SellerRepository sellerRepo;
 
-	@Autowired
+	@Mock
 	UserRepository userRepo;
 
-	@Autowired
+	@Mock
 	ProductRepository productRepo;
 
 	@InjectMocks
@@ -55,6 +68,9 @@ class SellerServiceTest {
 
 	@Mock
 	RoleRepository roleRepo;
+	
+	@Mock
+	EntityManager entityManager;
 
 	@Test
 	void testGetAllSeller_1() {
@@ -139,35 +155,290 @@ class SellerServiceTest {
 		assertThat(result.size()).isEqualTo(0);
 	}
 
-	void testGetSellerById() {
-		Long expectedId = (long) 2;
-		Seller expected = new Seller();
-		expected.setId(expectedId);
-
-		when(sellerRepo.findById(expectedId).get()).thenReturn(expected);
-		Seller result = sellerRepo.findById(expectedId).get();
-		assertThat(result.getId()).isEqualTo(expectedId);
+	@Test
+	public void testCreateNewSeller_1() {
+		// Arrange
+		String paypalEmail = "1";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		// Act
+		assertThrows(IllegalArgumentException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
 	}
-
-	void testgetSellerByAProduct() {
-		Seller seller = new Seller();
-		seller.setId((long) 1);
-
-		Product p1 = new Product();
-		p1.setId((long) 1);
-		p1.setSeller(seller);
-
-		Product p2 = new Product();
-		p2.setId((long) 2);
-		p2.setSeller(seller);
-
-		List<Product> expected = new ArrayList<>();
-		expected.add(p1);
-		expected.add(p2);
-
-		when(productRepo.findById((long) 1)).thenReturn(expected);
-		Seller result = sellerImpl.getSellerByAProduct((long) 1);
-
+	@Test
+	public void testCreateNewSeller_11() {
+		// Arrange
+		String paypalEmail = " ";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
 	}
-
+	
+	@Test
+	public void testCreateNewSeller_2() {
+		// Arrange
+		String paypalEmail = "r             @gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
+	@Test
+	public void testCreateNewSeller_3() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		// Act
+		boolean result = sellerImpl.createNewSeller(paypalEmail);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testCreateNewSeller_4() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		// Act
+		boolean result = sellerImpl.createNewSeller(paypalEmail);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testCreateNewSeller_5() {
+		// Arrange
+		String paypalEmail = "r            @gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
+	
+	@Test
+	public void testCreateNewSeller_6() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(any(Seller.class));
+		
+		assertThrows(InvalidUseOfMatchersException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
+	
+	@Test
+	public void testCreateNewSeller_7() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(any(Seller.class));
+		
+		assertThrows(InvalidUseOfMatchersException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
+	
+	
+	@Test
+	public void testCreateNewSeller_8() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		boolean result = sellerImpl.createNewSeller(paypalEmail);
+		assertTrue(result);
+	}
+	
+	
+	@Test
+	public void testCreateNewSeller_9() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(any(Seller.class));
+		
+		assertThrows(InvalidUseOfMatchersException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
+	
+	@Test
+	public void testCreateNewSeller_10() {
+		// Arrange
+		String paypalEmail = "paypal@gmail.com";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		boolean result = sellerImpl.createNewSeller(paypalEmail);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testCreateNewSeller_12() {
+		// Arrange
+		String paypalEmail = " ";
+		Account account = new Account();
+		account.setEmail("johndoe@gmail.com");
+		UserDetailsImpl userDetails = new UserDetailsImpl();
+		userDetails.setAccount(account);
+		account.setId(1L);
+		User user = new User(account);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		Mockito.when(authentication.getPrincipal()).thenReturn(userDetails);
+		SecurityContextHolder.setContext(securityContext);
+		
+		when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+		when(sellerRepo.findByPaypalEmail(paypalEmail)).thenReturn(null);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			sellerImpl.createNewSeller(paypalEmail);
+		});
+	}
 }
