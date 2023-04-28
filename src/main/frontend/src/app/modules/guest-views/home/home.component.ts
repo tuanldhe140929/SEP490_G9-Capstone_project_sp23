3edf96a4e823a2a83a75c53df6494cbbf1925b81
NnguyenCaoSon <sonnche150279@fpt.edu.vn>
2023-04-28 22:06:53 +0700

    HomeUpdate V1

--- src/main/frontend/src/app/modules/guest-views/home/home.component.ts
+++ src/main/frontend/src/app/modules/guest-views/home/home.component.ts
@@ -44,7 +44,6 @@ export class HomeComponent implements OnInit{
       return "https://lyon.palmaresdudroit.fr/images/joomlart/demo/default.jpg";
     }
   }
-
   redirectProductDetails(product:Product){
 	this.router.navigate(['/products', product.id]);
   }
@@ -63,7 +62,7 @@ export class HomeComponent implements OnInit{
     this.productService.getLastestUpdatedProductForHomePage().subscribe(
       data => {
         console.log(data);
-        this.lastestProductList = data;
+        this.lastestProductList = data.slice(0,4);
         for (let i = 0; i<this.lastestProductList.length; i++){
           this.lastestProductList[i].price = Number.parseFloat(this.lastestProductList[i].price.toFixed(1));
         }
@@ -118,6 +117,7 @@ export class HomeComponent implements OnInit{
       this.router.navigate(['collection/'+sellerId]);
     })
   }
+
   //Image slider//
   @Input() images: carouselImage[] = []
   @Input() indicators = true;
@@ -125,6 +125,6 @@ export class HomeComponent implements OnInit{
   selectedIndex = 0;
   //set index for image dot slider
   selectImage(index: number): void {
-    this.selectedIndex = index;
+    this.selectedIndex = index
   }
 }
